6a86dd1fa96b65b32c1009f6c691d724f6c4f38a
rational-architecture-1641 <jarmah241@gmail.com>
2020-09-30 16:48:51 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -27,8 +27,10 @@ end
 
 def return_string_array(array)
   counter = 0
+  new_array = []
   while array[counter] do
-    array[counter].to_s
+    new_array << array[counter].to_s
     counter += 1
   end
+  new array
 end
