03921c44861f95626ce2d459f78ed755e4728cad
rational-architecture-1641 <jarmah241@gmail.com>
2020-09-30 16:46:33 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,26 +7,3 @@ def loop_message_five_times(string)
     counter += 1
   end
 end
-
-def loop_message_n_times(string, num)
-  counter = 0
-  while counter <= num do
-    puts string
-    counter += 1
-  end
-end
-
-def output_array()
-  counter = 0
-  while array[couunter] do
-    puts array[counter]
-    counter += 1
-  end
-end
-
-def return_string_array()
-  counter = 0
-  while array[counter] do
-    array[counter].to_s
-    counter += 1
-end
